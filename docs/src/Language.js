03a43d46899129293a5f66e32e915a34a857f087
luisboto <43352462+LuisBoto@users.noreply.github.com>
2022-05-28 00:24:16 +0200

    On the fly language swithing

---
+++ docs/src/Language.js
@@ -0,0 +1,66 @@
+class Language {
+
+    static TITLE_DESCRIPTOR = new Language([
+        "Ingeniero de Software", 
+        "Software Engineer"
+    ]);
+    static MASTERS_NAME = new Language([
+        "Cloud Apps: Desarrollo y despliegue de aplicaciones en la nube", 
+        "Cloud Apps: Development and deployment of cloud based applications"
+    ]);
+    static MASTERS_LOCATION = new Language([
+        "Universidad Rey Juan Carlos, Máster",
+        "King Juan Carlos University, Master's Degree"
+    ]);
+    static PRACTICES_POSITION = new Language([
+        "Desarrollador Júnior", 
+        "Junior Developer"
+    ]);
+    static PRACTICES_LOCATION = new Language([
+        "Capgemini Asturias, Prácticas de empresa", 
+        "Capgemini Asturias, Internship"
+    ]);
+    static DEGREE_NAME = new Language([
+        "Ingeniería Informática del Software", 
+        "Software Engineering"
+    ]);
+    static DEGREE_LOCATION = new Language([
+        "Universidad de Oviedo, Grado",
+        "University of Oviedo, Bachelor's Degree"
+    ]);
+
+    constructor(text) {
+        this.text = text;
+    }
+
+    getText() {
+        return this.text[Language.currentLanguage];
+    }
+
+}
+
+let spanish = 0;
+let english = 1;
+Language.currentLanguage = english;
+
+function alternateLanguage() {
+    Language.currentLanguage = 
+        (Language.currentLanguage === spanish ? english : spanish);
+    
+    document.getElementById("titleDescriptor").innerHTML = 
+        Language.TITLE_DESCRIPTOR.getText();
+    document.getElementById("mastersName").innerHTML = 
+        Language.MASTERS_NAME.getText();
+    document.getElementById("mastersLocation").innerHTML = 
+        Language.MASTERS_LOCATION.getText();
+    document.getElementById("practicesPosition").innerHTML = 
+        Language.PRACTICES_POSITION.getText();
+    document.getElementById("practicesLocation").innerHTML = 
+        Language.PRACTICES_LOCATION.getText();
+    document.getElementById("degreeName").innerHTML = 
+        Language.DEGREE_NAME.getText();
+    document.getElementById("degreeLocation").innerHTML = 
+        Language.DEGREE_LOCATION.getText();
+}
+
+alternateLanguage();
